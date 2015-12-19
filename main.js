403e591ea3f895696e1e0defcdd76719e6666c1a
Paul Chin Jr <pchinjr@gmail.com>
2015-12-19 07:06:50 +0000

    toggle attack buttons on end turn

--- main.js
+++ main.js
@@ -1,39 +1,21 @@
 var player1 = { dom: $('.player-1'), health:100, dice: 1};
 var player2 = { dom: $('.player-2'), health:100, dice: 1 };
 
-//roll a single die
-function rollDie(sides) {
-   return( Math.floor (Math.random() * sides));
-}
-
-//roll multiple dice
-function rollDice(number, sides) {
-    var total = 1;
-    while(number-- > 0) total += rollDie(sides);
-    return total;
-}
-
-//roll the player's dice and show them on the screen
+//roll the player's dice and show results of individual dice on the screen
 function dieroll(player) { 
-  var result = [];  
-  var dice = [];
+  var result = []; //empty array to hold the results of the individual die rolls
+  var dice = []; //empty array to hold the unicode dice characters
   for (var i = 0; i < player.dice ; i++){
     result.push( Math.floor(Math.random() * 6));
-  }
+  } //rolls the dice
   for (var d = 0; d < result.length; d++){
     dice.push("&#x268" + result[d] + ";");
-  }
-  player.dom.find('.showDice').html(dice);  
-  var sum = result.reduce (function(a,b) {
+  } //creates the unicode dice characters
+  player.dom.find('.showDice').html(dice);  //displays the dice 
+  var sum = result.reduce (function(a,b) {  
     return a + b;
-  });  
-  player.dom.find('.currentRoll, .attack, .takeDamage').html( sum + (1*dice.length) );
-}
-
-
-//gameplay roll
-function roll(player) {
-    player.dom.find('.currentRoll, .attack, .takeDamage').html( rollDice(player.dice, 6));
+  }); // adds the dice together   
+  player.dom.find('.currentRoll, .attack, .takeDamage').html( sum + (1*dice.length) ); // I had to add 1*the number of dice because the unicode starts at 0 to represent the number 1.
 }
 
 //attack other player
@@ -68,13 +50,13 @@ function checkWinner() {
 function endTurn(player) {
     
     if(player === 'player1') {
-        player1.dom.find('.roll, .clickAttack, .clickTakeDamage').prop("disabled", true);
-        player2.dom.find('.roll, .clickAttack, .clickTakeDamage').prop("disabled", false);
-        player1.dom.find('.currentRoll, .attack, .takeDamage').html("_");
+        player1.dom.find('.roll').prop("disabled", true);
+        player2.dom.find('.roll').prop("disabled", false);
+        player1.dom.find('.clickAttack, .clickTakeDamage').toggleClass('hide');
     } else {
-        player2.dom.find('.roll, .clickAttack, .clickTakeDamage').prop("disabled", true);
-        player1.dom.find('.roll, .clickAttack, .clickTakeDamage').prop("disabled", false);
-        player2.dom.find('.currentRoll, .attack, .takeDamage').html("_");
+        player2.dom.find('.roll').prop("disabled", true);
+        player1.dom.find('.roll').prop("disabled", false);
+        player2.dom.find('.clickAttack, .clickTakeDamage').toggleClass('hide');
     }
 }
 
@@ -82,11 +64,13 @@ function endTurn(player) {
 player1.dom.find(".roll").on( 'click', function() {
     dieroll(player1);
     player1.dom.find('.roll').prop("disabled", true);
+    player1.dom.find('.clickAttack, .clickTakeDamage').toggleClass('hide');
 });
 
 player2.dom.find(".roll").on( 'click', function() {
     dieroll(player2);
     player2.dom.find('.roll').prop("disabled", true);
+    player2.dom.find('.clickAttack, .clickTakeDamage').toggleClass('hide');
 });
 
 //players attack
@@ -111,12 +95,4 @@ player1.dom.find(".clickTakeDamage").on( 'click', function() {
 player2.dom.find(".clickTakeDamage").on( 'click', function() {
     takeDamage( player2, parseInt( player2.dom.find('.takeDamage').text(), 10) );
     endTurn('player2');
-});
-
-
-
-
-$(".newroll").on( 'click', function() {
-    dieroll(player1);
-    player1.dom.find('.roll').prop("disabled", true);
 });
