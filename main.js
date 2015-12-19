53d955361743ca9fd26c386aa70510c9192f532e
Paul Chin Jr <pchinjr@gmail.com>
2015-12-19 05:44:14 +0000

    added health bars

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var player1 = { dom: $('.player-1'), health:30, dice: 1};
-var player2 = { dom: $('.player-2'), health:30, dice: 1 };
+var player1 = { dom: $('.player-1'), health:100, dice: 1};
+var player2 = { dom: $('.player-2'), health:100, dice: 1 };
 
 //roll a single die
 function rollDie(sides) {
-   return(Math.floor(Math.random() * sides));
+   return( Math.floor (Math.random() * sides));
 }
 
 //roll multiple dice
@@ -12,6 +12,24 @@ function rollDice(number, sides) {
     while(number-- > 0) total += rollDie(sides);
     return total;
 }
+
+//roll the player's dice and show them on the screen
+function dieroll(player) { 
+  var result = [];  
+  var dice = [];
+  for (var i = 0; i < player.dice ; i++){
+    result.push( Math.floor(Math.random() * 6));
+  }
+  for (var d = 0; d < result.length; d++){
+    dice.push("&#x268" + result[d] + ";");
+  }
+  player.dom.find('.showDice').html(dice);  
+  var sum = result.reduce (function(a,b) {
+    return a + b;
+  });  
+  player.dom.find('.currentRoll, .attack, .takeDamage').html( sum + (1*dice.length) );
+}
+
 
 //gameplay roll
 function roll(player) {
@@ -22,6 +40,7 @@ function roll(player) {
 function attack(player, hit) {
     player.health = player.health - hit;
     player.dom.find('.health').html(player.health);
+    player.dom.find('.healthBar').css('width', player.health+'%');
     checkWinner();
 }
 
@@ -29,6 +48,7 @@ function attack(player, hit) {
 function takeDamage(player, hit) {
     player.health = player.health - hit;
     player.dom.find('.health').html(player.health);
+    player.dom.find('.healthBar').css('width', player.health+'%');
     player.dice = player.dice + 1;
     player.dom.find('.dice').html(player.dice);
     
@@ -60,12 +80,12 @@ function endTurn(player) {
 
 //players roll
 player1.dom.find(".roll").on( 'click', function() {
-    roll(player1);
+    dieroll(player1);
     player1.dom.find('.roll').prop("disabled", true);
 });
 
 player2.dom.find(".roll").on( 'click', function() {
-    roll(player2);
+    dieroll(player2);
     player2.dom.find('.roll').prop("disabled", true);
 });
 
@@ -91,4 +111,12 @@ player1.dom.find(".clickTakeDamage").on( 'click', function() {
 player2.dom.find(".clickTakeDamage").on( 'click', function() {
     takeDamage( player2, parseInt( player2.dom.find('.takeDamage').text(), 10) );
     endTurn('player2');
+});
+
+
+
+
+$(".newroll").on( 'click', function() {
+    dieroll(player1);
+    player1.dom.find('.roll').prop("disabled", true);
 });
